cd5d624367eefbc9d9815d949dbe99901ed0faee
ETatskyi <halkroga@gmail.com>
2022-08-15 11:54:40 +0300

    15.08.22_11:55

--- hw18/script.js
+++ hw18/script.js
@@ -1,13 +1,8 @@
 function palindrome(str) {
-    str = str.replaceAll(' ', '').toLowerCase().split('')
-    return str.join('') === str.reverse().join('')
+    return str.replaceAll(' ', '').toLowerCase().split('').join('') === str.replaceAll(' ', '').toLowerCase().split('').reverse().join('')
 }
 
 console.log(palindrome('racecar')); // true
 console.log(palindrome('table')); // false
 console.log(palindrome('Анна')); // true
 console.log(palindrome('А роза упала на лапу Азора')); // true
-
-
-// str = str.replaceAll(' ', '').toLowerCase()
-// return str.split('').join('') === str.split('').reverse().join('')
